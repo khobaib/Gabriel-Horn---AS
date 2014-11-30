79e910ffea938f5c76f1c05ddbf1203046ebbdba
sbrishta <sbrishta>
2014-11-30 18:52:29 +0600

    The nest on cherry street child app created

---
+++ app/src/main/java/org.thenest.app/adapter/RewardListAdapter.java
@@ -0,0 +1,40 @@
+package org.thenest.app.adapter;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import org.thenest.app.R;
+import org.thenest.app.model.LocalUser;
+import org.thenest.app.model.Reward;
+import org.thenest.app.viewHelpers.BaseParseArrayAdapter;
+import org.thenest.app.viewHelpers.ViewHolder;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
+public class RewardListAdapter extends BaseParseArrayAdapter<Reward>
+{
+    public RewardListAdapter(Context context, int itemViewResource)
+    {
+        super(context, new ParseQueryAdapter.QueryFactory()
+        {
+            @Override
+            public ParseQuery create()
+            {
+                ParseQuery<Reward> query = ParseQuery.getQuery("Rewards");
+                query.whereEqualTo("appCompany", LocalUser.getInstance().getParentCompany());
+                query.addAscendingOrder("pointsNeeded");
+                return query;
+            }
+        }, itemViewResource);
+    }
+
+    @Override
+    protected void initView(ViewHolder holder, Reward data)
+    {
+        TextView pointsNeeded = (TextView) holder.getView(R.id.reward_points_text_view);
+        TextView rewardName = (TextView) holder.getView(R.id.reward_name_text_view);
+
+        pointsNeeded.setText(Integer.toString(data.getPointsNeeded()));
+        rewardName.setText(data.getName());
+    }
+}
