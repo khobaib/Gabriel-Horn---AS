9cd7c0c13e3511613dd9087a364cebb2e6703705
sbrishta <sbrishta>
2014-11-30 16:52:05 +0600

    Child app Emerge Medical Day Spa created

---
+++ app/src/main/java/org.emergespa.app/fragments/VisitSiteFragment.java
@@ -0,0 +1,33 @@
+package org.emergespa.app.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import org.emergespa.app.R;
+import org.emergespa.app.model.LocalUser;
+
+public class VisitSiteFragment extends Fragment
+{
+    private WebView vsWebView;
+
+    public static VisitSiteFragment newInstance()
+    {
+        return new VisitSiteFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.layout_tc_fragment, container, false);
+        vsWebView = (WebView) v.findViewById(R.id.wv_tc);
+        String url = LocalUser.getInstance().getParentCompany().getString("websiteUrl");
+        if (!url.equals(""))
+            vsWebView.loadUrl(url);
+        return v;
+    }
+}
---
+++ app/src/main/java/org.emergespa.app/interfaces/ActivityResultListener.java
@@ -0,0 +1,11 @@
+package org.emergespa.app.interfaces;
+
+import android.content.Intent;
+
+/**
+ * Created by Usama on 10/4/14.
+ */
+public interface ActivityResultListener
+{
+    public void onActivityResult(int requestCode, int resultCode, Intent intent);
+}
